3b65bf196b6c57d0cb82a4b81171b688792d4c94
VCMM20 <89882529+VCMM20@users.noreply.github.com>
2026-02-08 23:41:16 -0500

    Fix headers license and CI

--- common/data/src/test/java/org/thingsboard/server/common/data/TemperatureConverterTest.java
+++ common/data/src/test/java/org/thingsboard/server/common/data/TemperatureConverterTest.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,8 @@
  */
 package org.thingsboard.server.common.data;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TemperatureConverterTest {
 
